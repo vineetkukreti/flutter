69c3a49f88eac3bf85264e4aa342629a71351e97
vineetkukreti <91364632+vineetkukreti@users.noreply.github.com>
2022-05-07 19:41:14 +0530

    Add files via upload

---
+++ allAboutString.dart
@@ -0,0 +1,20 @@
+void main() {
+//   String name = "vineet ";
+//   String subname = "Kukreti";
+//   String fullname = name + subname;
+//   length of string
+//   print(fullname.length);
+
+//   trim the string
+//   String name = "     vineet kukreti    ";
+//   print(name.trim());
+
+//   split the string
+//   String name = "Vineet kukreti";
+//   print(name.split(' '));
+
+  
+  // replace a chunck of string
+  String name = "Vineet kukreti";
+  print(name.replaceAll('Vineet', 'i love you'));
+}
