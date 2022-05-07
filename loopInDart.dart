41a7f2fdd009d67038633f006053f07ec3d739b7
vineetkukreti <91364632+vineetkukreti@users.noreply.github.com>
2022-05-07 20:02:32 +0530

    Add files via upload

---
+++ loopInDart.dart
@@ -0,0 +1,28 @@
+void main()
+{
+ // types of data type in dart
+ // Dart is composition of c++,java,python
+ // if you are familiar with any of these lanngauage 
+ // you will clearly find that out
+  
+  // So today we are going to see more about dart
+  // In dart we will be dicussing about loops
+  
+  int n = 10;
+  for(int i=0;i<n;i++)
+     print("hello world ${1+i}");
+  // we use both ' ' or " " inside the print function
+  
+  //we can also use break and continue statment in just like we used to other language 
+  // let's see hoe they work with some example.
+  int n = 10;
+  for(int i=1;i<=n;i++)
+  {
+      if(i==5)
+      {
+        print("hello world ${i}");
+        break;
+       }
+   }
+     
+ }
