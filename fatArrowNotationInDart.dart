127b869d75f2b9f1da40d8550a78ca8e94494f8f
vineet kukreti <91364632+vineetkukreti@users.noreply.github.com>
2022-05-08 14:53:07 +0530

    Add files via upload

---
+++ fatArrowNotationInDart.dart
@@ -0,0 +1,10 @@
+// fat arrow notation
+// for a function that contains just one expression
+// you can use a shorthand syntax
+// The => notation is sometimes refered to as arrow syntax
+
+void main()
+{
+  print(add(10,50));
+}
+int add(int a ,int b) => a+b;
---
+++ functionInDart.dart
@@ -0,0 +1,29 @@
+// Function are just a piece of code which perform some
+// specific task.
+
+// Types of function
+// 1. No argument with no return type
+// 2. No argument with return type
+// 3. Argument with no return type
+// 4. Argument with return type
+
+// void main()
+// {
+//   waiter("cold drink","milk",500);
+// }
+//  waiter(String drinks,String drinks2,int amt)
+//  {
+//    print("Order is $drinks ,$drinks2 and It would be under $amt");
+//  }
+
+// void main()
+// {
+//   int c = add(5,3);
+//   print("c = $c");
+// }
+// int add(int a,int b)
+// {
+//   return a+b;
+// }
+
+
